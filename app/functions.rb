604a3aadd9b016ca4dc98230eedf36aa89228d21
Jay <jayshaw911@hotmail.com>
2014-03-14 11:52:35 -0500

    added tables

--- app/functions.rb
+++ app/functions.rb
@@ -9,29 +9,44 @@ ActiveRecord::Base.logger = Logger.new(STDERR)
 ActiveRecord::Schema.define do
   unless ActiveRecord::Base.connection.tables.include? 'visits'
     create_table :visits do |table|
-      table.column :name, :string
-      table.column :description, :string
+      table.column :country, :string
+      table.column :image, :string
+      table.column :article_h, :string
+      table.column :article_b, :string
+      table.column :wiki_h, :string
+      table.column :article_b, :string
     end
   end
   
   unless ActiveRecord::Base.connection.tables.include? 'comments'
     create_table :comments do |table|
-      table.column :user_id, :integer
+      table.column :visit_id, :integer
       table.column :name, :string
+      table.column :comment, :string
     end
   end
 end
 
-class Visits < ActiveRecord::Base
+class Visit < ActiveRecord::Base
   # name - string
   # description - string
   
   has_many :comments
-  # 1. There is a table called "friends".
+  # 1. There is a table called "comments".
   # 2. That table has a foreign_key that refers to this table.
-  # 3. That foreign_key is called "this_table_singular_id". ("user_id")
+  # 3. That foreign_key is called "visit_id".
   # 4. There may be many rows in that table that refer to one row in this table.
   
   # Now...
   # @user.friends returns an Array of Friend objects.
+end
+
+class Comment < ActiveRecord::Base
+  belongs_to :visits
+  # 1. There is a table called "visits".
+  # 2. That table has a field called "id".
+  # 3. The "visit_id" of this table's row matches some "id" in "visits".
+  
+  # Now...
+  # @friend.user returns a single User object.
 end
