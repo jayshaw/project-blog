6fae88e5c5800214418f2df5b7762938e7d314b6
Jay <jayshaw911@hotmail.com>
2014-03-14 21:03:50 -0500

    added new entry page

--- app/main.rb
+++ app/main.rb
@@ -10,6 +10,24 @@ class TravelBlog < Sinatra::Base
     erb :home
   end
   
+  get "/view" do
+    erb :blog_view
+  end
+  
+  post "/save_attributes" do
+      title = params[:title]
+      image = params[:image]
+      article_header = params[:article_h]
+      article_body = params[:article_b]
+      wiki_header = params[:wiki_h]
+      wiki_body = params[:wiki_b]
+  
+      user = Visits.new({:title => title, :image => image, :article_h => article_header, :article_b => article_body, :wiki_h => wiki_header,
+      :wiki_b => wiki_body})
+      user.save # Saves the object to the database table.
+      redirect to("/")
+  end
+  
   get "/australia" do
     erb :australia_trip
   end
